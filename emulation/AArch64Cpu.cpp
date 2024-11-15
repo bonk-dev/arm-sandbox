9fe7f64aa92bf56c8f4156010c011c16d9b6c773
bonk-dev <github.p0j6t@passinbox.com>
2024-11-15 20:23:21 +0100

    Rename "reg_t" to "regindex_t"

--- emulation/AArch64Cpu.cpp
+++ emulation/AArch64Cpu.cpp
@@ -9,18 +9,18 @@ AArch64Cpu::AArch64Cpu() {
         AARCH64_GENERAL_PURPOSE_REGISTERS);
 }
 
-void AArch64Cpu::write_gp_register_32(const reg_t index, const uint32_t val) {
+void AArch64Cpu::write_gp_register_32(const regindex_t index, const uint32_t val) {
     this->_general_registers[index] = val;
 }
 
-void AArch64Cpu::write_gp_register_64(const reg_t index, const uint64_t val) {
+void AArch64Cpu::write_gp_register_64(const regindex_t index, const uint64_t val) {
     this->_general_registers[index] = val;
 }
 
-uint32_t AArch64Cpu::read_gp_register_32(const reg_t index) const {
+uint32_t AArch64Cpu::read_gp_register_32(const regindex_t index) const {
     return this->_general_registers[index];
 }
 
-uint64_t AArch64Cpu::read_gp_register_64(const reg_t index) const {
+uint64_t AArch64Cpu::read_gp_register_64(const regindex_t index) const {
     return this->_general_registers[index];
 }
--- emulation/AArch64Cpu.h
+++ emulation/AArch64Cpu.h
@@ -4,7 +4,7 @@
 #include <vector>
 
 constexpr size_t AARCH64_GENERAL_PURPOSE_REGISTERS = 32;
-typedef uint8_t reg_t;
+typedef uint8_t regindex_t;
 
 class AArch64Cpu {
 private:
@@ -12,9 +12,9 @@ private:
 public:
     AArch64Cpu();
 
-    void write_gp_register_32(reg_t index, uint32_t val);
-    void write_gp_register_64(reg_t index, uint64_t val);
+    void write_gp_register_32(regindex_t index, uint32_t val);
+    void write_gp_register_64(regindex_t index, uint64_t val);
 
-    [[nodiscard]] uint32_t read_gp_register_32(reg_t index) const;
-    [[nodiscard]] uint64_t read_gp_register_64(reg_t index) const;
+    [[nodiscard]] uint32_t read_gp_register_32(regindex_t index) const;
+    [[nodiscard]] uint64_t read_gp_register_64(regindex_t index) const;
 };
