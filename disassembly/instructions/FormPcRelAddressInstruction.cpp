a0b1fe10ebcf4291b33b7cbf6a03fc442bd585f5
bonk-dev <github.p0j6t@passinbox.com>
2024-11-18 09:12:59 +0100

    Make decode_immediate a class method

--- disassembly/instructions/FormPcRelAddressInstruction.cpp
+++ disassembly/instructions/FormPcRelAddressInstruction.cpp
@@ -1,7 +1,7 @@
 #include "FormPcRelAddressInstruction.h"
 #include "../../numbers/two_complement.h"
 
-int32_t decode_immediate(uint32_t encoded) {
+int32_t FormPcRelAddressInstruction::decode_immediate(uint32_t encoded) {
 	const uint32_t imm_lo = encoded >> 29 & 0b11;
 	const uint32_t imm_hi = encoded >> 5 & 0b1111111111111111111; // 19 bits
 	const uint32_t joined_imm = (imm_hi << 2) | imm_lo;
--- disassembly/instructions/FormPcRelAddressInstruction.h
+++ disassembly/instructions/FormPcRelAddressInstruction.h
@@ -3,6 +3,9 @@
 #include "../../emulation/emu_types.h"
 
 typedef struct FormPcRelAddressInstruction {
+private:
+	static int32_t decode_immediate(uint32_t encoded);
+public:
 	bool rel_to_4kb_page;
 
 	regindex_t destination_reg_index;
