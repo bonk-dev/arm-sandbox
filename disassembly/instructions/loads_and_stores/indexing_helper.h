8e36afc7377555185aeb9ade076e8002bc4d2564
bonk-dev <github.p0j6t@passinbox.com>
2024-11-23 20:16:03 +0100

    Create a helper function for load/store indexing

---
+++ disassembly/instructions/loads_and_stores/indexing_helper.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <cstdint>
+#include "../IndexingMode.h"
+#include "../../../emulation/AArch64Cpu.h"
+
+namespace InstructionDefs::IndexingHelpers {
+	template <class T>
+	uintptr_t calc_next_address(
+			InstructionDefs::IndexingMode mode,
+			std::shared_ptr<AArch64Cpu>& cpu,
+			T immediate,
+			regindex_t base_reg,
+			bool is_wide = true
+	) {
+		uintptr_t virt_addr = is_wide
+							  ? cpu->read_gp_register_64(base_reg)
+							  : cpu->read_gp_register_32(base_reg);
+
+		switch (mode) {
+			case InstructionDefs::IndexingMode::NonTemporalOffset:
+			case InstructionDefs::IndexingMode::SignedOffset:
+				virt_addr += immediate;
+				break;
+			case InstructionDefs::IndexingMode::PostIndex:
+				cpu->write_gp_register_64(base_reg, virt_addr + immediate);
+				break;
+			case InstructionDefs::IndexingMode::PreIndex:
+				virt_addr += immediate;
+				cpu->write_gp_register_64(base_reg, virt_addr);
+				break;
+			default:
+				throw std::runtime_error("Illegal indexing mode");
+		}
+
+		return virt_addr;
+	}
+}
--- emulation/executors/LoadStoreRegPairExecutor.cpp
+++ emulation/executors/LoadStoreRegPairExecutor.cpp
@@ -1,31 +1,7 @@
 #include "LoadStoreRegPairExecutor.h"
+#include "../../disassembly/instructions/loads_and_stores/indexing_helper.h"
 
 LoadStoreRegPairExecutor::LoadStoreRegPairExecutor(const std::shared_ptr<AArch64Cpu> &cpu) : ExecutorBase(cpu) {}
-
-uintptr_t LoadStoreRegPairExecutor::calc_next_address(InstructionDefs::LoadsAndStores::LoadStoreRegisterPairInstruction& instruction) {
-	const auto cpu = this->get_cpu().get();
-	uintptr_t virt_addr = instruction.is_wide
-						  ? cpu->read_gp_register_64(instruction.base_reg)
-						  : cpu->read_gp_register_32(instruction.base_reg);
-
-	switch (instruction.encoding) {
-		case InstructionDefs::IndexingMode::NonTemporalOffset:
-		case InstructionDefs::IndexingMode::SignedOffset:
-			virt_addr += instruction.immediate_value;
-			break;
-		case InstructionDefs::IndexingMode::PostIndex:
-			cpu->write_gp_register_64(instruction.base_reg, virt_addr + instruction.immediate_value);
-			break;
-		case InstructionDefs::IndexingMode::PreIndex:
-			virt_addr += instruction.immediate_value;
-			cpu->write_gp_register_64(instruction.base_reg, virt_addr);
-			break;
-		default:
-			throw std::runtime_error("Illegal indexing mode");
-	}
-
-	return virt_addr;
-}
 
 void LoadStoreRegPairExecutor::execute(InstructionDefs::LoadsAndStores::LoadStoreRegisterPairInstruction& instruction) {
 	if (instruction.is_simd) {
@@ -36,7 +12,13 @@ void LoadStoreRegPairExecutor::execute(InstructionDefs::LoadsAndStores::LoadStor
 
 	const uint8_t reg_indexes[2] = {instruction.first_reg_index, instruction.second_reg_index};
 	for (uint8_t reg_index : reg_indexes) {
-		const uintptr_t virtual_address = this->calc_next_address(instruction);
+		const auto virtual_address = InstructionDefs::IndexingHelpers::calc_next_address(
+				instruction.encoding,
+				this->get_cpu(),
+				instruction.immediate_value,
+				instruction.base_reg,
+				instruction.is_wide);
+
 		if (instruction.is_load) {
 			if (instruction.is_wide) {
 				cpu->write_gp_register_64(reg_index, cpu->get_memory().read_uint64(virtual_address));
