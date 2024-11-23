4f0f4675ca52dd7f525bb8127597f5561c474fbb
bonk-dev <github.p0j6t@passinbox.com>
2024-11-23 18:06:34 +0100

    Simplify is_using_64bit_reg init

--- disassembly/instructions/loads_and_stores/LoadStoreRegUnsignedImm.cpp
+++ disassembly/instructions/loads_and_stores/LoadStoreRegUnsignedImm.cpp
@@ -3,7 +3,7 @@
 InstructionDefs::LoadsAndStores::LoadStoreRegUnsignedImm::LoadStoreRegUnsignedImm(uint32_t encoded) :
 	size(8 << (encoded >> 30)),
 	is_simd((encoded >> 26) & 1),
-	is_using_64bit_reg((encoded >> 23) & 0b11),
+	is_using_64bit_reg((size == 64)),
 	base_reg((encoded >> 5) & 0b11111),
 	src_dst_reg(encoded & 0b11111),
 	unsigned_imm((encoded >> 10) & 0b111111111111) {}
