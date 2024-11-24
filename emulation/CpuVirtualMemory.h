d316dfaa99342d7ce70e02312d562fd3c6cfbbd4
bonk-dev <github.p0j6t@passinbox.com>
2024-11-24 20:47:33 +0100

    Add a templated read method

--- emulation/CpuVirtualMemory.h
+++ emulation/CpuVirtualMemory.h
@@ -2,6 +2,8 @@
 
 #include <cstdio>
 #include <cstdint>
+#include <sstream>
+#include <iostream>
 
 /**
  * @class CpuVirtualMemory
@@ -23,6 +25,19 @@ public:
 
 	void write(uintptr_t addr, uint32_t value);
 	void write(uintptr_t addr, uint64_t value);
+
+	template<class T>
+	T read(uintptr_t addr) {
+		std::stringstream ss;
+		ss << "[Memory] ";
+		ss << sizeof(T) * 8;
+		ss << "bit Read from address: " << std::hex << std::showbase << addr;
+
+		// std::cout will be replaced
+		std::cout << ss.str().c_str() << std::endl;
+
+		return 0;
+	}
 
 	[[nodiscard]] uint32_t read_uint32(uintptr_t addr);
 	[[nodiscard]] uint64_t read_uint64(uintptr_t addr);
