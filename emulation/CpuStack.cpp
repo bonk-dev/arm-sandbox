d0b7b8569c05806cb7ffd966dce85370e6755c52
bonk-dev <github.p0j6t@passinbox.com>
2024-12-05 21:28:36 +0100

    Add push and pop functions

--- emulation/CpuStack.cpp
+++ emulation/CpuStack.cpp
@@ -3,3 +3,7 @@
 CpuStack::CpuStack(size_t stackSize):
 	_stackMemory(std::make_unique<std::vector<std::byte>>(stackSize)),
 	_stackPointer(stackSize) {}
+
+void CpuStack::pop(size_t size) {
+	this->_stackPointer += size;
+}
--- emulation/CpuStack.h
+++ emulation/CpuStack.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <vector>
+#include <stdexcept>
 #include <memory>
+#include "emu_types.h"
 
 class CpuStack {
 private:
@@ -9,4 +11,19 @@ private:
 	size_t _stackPointer;
 public:
 	explicit CpuStack(size_t stackSize);
+
+	template <typename T>
+	void push(T value) {
+		this->_stackPointer -= sizeof(T);
+
+		const size_t vectorOffset = this->_stackMemory->size() - this->_stackPointer;
+		const T* ptr = reinterpret_cast<T*>(this->_stackMemory->data() + vectorOffset);
+		*ptr = value;
+	}
+
+	template <typename T>
+	void pop() {
+		this->pop(sizeof(T));
+	}
+	void pop(size_t size);
 };
