1f9e4516f02856bb454c4437c1bd1138a43edc24
bonk-dev <github.p0j6t@passinbox.com>
2024-12-10 11:24:25 +0100

    Print disassembly when decoding

--- emulation/executors/ExecutorBase'.h
+++ emulation/executors/ExecutorBase'.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ExecutorBase.h"
+#include "../../disassembly/disassembly.h"
 
 template <class InstructionDetailsT>
 class ExecutorBaseT : public ExecutorBase {
@@ -10,6 +11,8 @@ public:
 	virtual void execute(const InstructionDetailsT& details) = 0;
 	void decodeAndExecute(uint32_t encoded) override {
 		const auto details = InstructionDetailsT(encoded);
+		// TODO: Print conditionally
+		std::cout << disassembly::to_pretty_string(details) << std::endl;
 		this->execute(details);
 	}
 };
