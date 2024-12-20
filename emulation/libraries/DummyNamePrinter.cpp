770da14471b7372ffef75a0b23734c5c0d6102f6
bonk-dev <github.p0j6t@passinbox.com>
2024-12-20 21:01:45 +0100

    Implement DummyNamePrinter

--- emulation/libraries/DummyNamePrinter.cpp
+++ emulation/libraries/DummyNamePrinter.cpp
@@ -1,5 +1,10 @@
 #include "DummyNamePrinter.h"
 
 namespace Emulation::Libraries {
+	void DummyNamePrinter::execute() {
+		std::cout << "[DummyNamePrinter] Symbol called: " << this->_symbolName << std::endl;
+	}
 
+	DummyNamePrinter::DummyNamePrinter(const char *symbolName, [[maybe_unused]] const std::shared_ptr<AArch64Cpu> &cpu) :
+		_symbolName(symbolName) {}
 }
--- emulation/libraries/DummyNamePrinter.h
+++ emulation/libraries/DummyNamePrinter.h
@@ -5,7 +5,10 @@
 
 namespace Emulation::Libraries {
     class DummyNamePrinter {
-        DummyNamePrinter(const char* symbolName, const std::shared_ptr<AArch64Cpu>& cpu);
+	private:
+		std::string _symbolName;
+	public:
+        DummyNamePrinter(const char* symbolName, [[maybe_unused]] const std::shared_ptr<AArch64Cpu>& cpu);
         void execute();
     };
 }
