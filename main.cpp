f99c04fb8c67530f8c5463275e51935e30a3e9b4
bonk-dev <github.p0j6t@passinbox.com>
2024-12-20 21:40:34 +0100

    Add UDF #1, #0

--- main.cpp
+++ main.cpp
@@ -45,6 +45,9 @@ std::map<InstructionType, std::unique_ptr<ExecutorBase>> map_all_executors(const
 
 int prototype_main() {
 	std::vector<std::byte> sample_code = {
+			// UDF #1 (special library call), #1 (symbol 0)
+			std::byte(0x00), std::byte(0x00), std::byte(0x01), std::byte(0x00),
+
 			// UDF #1 (special library call), #1 (symbol 1)
 			std::byte(0x01), std::byte(0x00), std::byte(0x01), std::byte(0x00),
 
