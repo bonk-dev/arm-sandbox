698d2cb1ed14445f85158117352fea312b56a31e
bonk-dev <github.p0j6t@passinbox.com>
2024-12-20 21:27:15 +0100

    Make the DummyNamePrinter derive from the EmulatedSymbol class

--- emulation/libraries/DummyNamePrinter.h
+++ emulation/libraries/DummyNamePrinter.h
@@ -1,14 +1,14 @@
 #pragma once
 #include <memory>
-
 #include "../AArch64Cpu.h"
+#include "EmulatedSymbol.h"
 
 namespace Emulation::Libraries {
-    class DummyNamePrinter {
+    class DummyNamePrinter : public EmulatedSymbol {
 	private:
 		std::string _symbolName;
 	public:
         DummyNamePrinter(const char* symbolName, [[maybe_unused]] const std::shared_ptr<AArch64Cpu>& cpu);
-        void execute();
+        void execute() override;
     };
 }
