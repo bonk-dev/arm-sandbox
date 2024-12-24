1420671ee5f2f6f4da242fbf750ea787cbbe053a
Dawid <dawid@dpago.dev>
2024-12-24 20:25:38 +0100

    Make ReservedCallExecutor take in a Mapper in its ctor

--- emulation/executors/reserved/ReservedCallExecutor.cpp
+++ emulation/executors/reserved/ReservedCallExecutor.cpp
@@ -1,21 +1,24 @@
 #include "ReservedCallExecutor.h"
-#include "../../emul_registers.h"
 
 Executors::Reserved::ReservedCallExecutor::ReservedCallExecutor(const std::shared_ptr<AArch64Cpu> &cpu)
 	: ExecutorBaseT<InstructionDefs::Reserved::ReservedCall>(cpu) {}
+
+Executors::Reserved::ReservedCallExecutor::ReservedCallExecutor(const std::shared_ptr<AArch64Cpu> &cpu,
+                                                                const std::shared_ptr<Emulation::Libraries::Mapper> &mapper)
+	: ExecutorBaseT<InstructionDefs::Reserved::ReservedCall>(cpu),
+	_mapper(mapper) {}
 
 void Executors::Reserved::ReservedCallExecutor::execute(const InstructionDefs::Reserved::ReservedCall &instruction) {
 	switch (instruction.call_type) {
 		case InstructionDefs::Reserved::ReservedCalls::LibraryCall: {
 			std::cout << "[ReservedCallExecutor] Calling symbol " << instruction.immediate << std::endl;
 
-			auto cpu = this->get_cpu().get();
-			if (!cpu->getMapper().hasLibraryImplementation(instruction.immediate)) {
+			if (!_mapper->hasLibraryImplementation(instruction.immediate)) {
 				std::cerr << "[ReservedCallExecutor] No library implementation was found for symbol #" << instruction.immediate << std::endl;
 				throw std::runtime_error("No lib implementation was found");
 			}
 			else {
-				auto& impl = cpu->getMapper().getLibraryImplementation(instruction.immediate);
+				auto& impl = _mapper->getLibraryImplementation(instruction.immediate);
 				impl.execute();
 			}
 
--- emulation/executors/reserved/ReservedCallExecutor.h
+++ emulation/executors/reserved/ReservedCallExecutor.h
@@ -2,11 +2,15 @@
 
 #include "../../../disassembly/instructions/reserved/ReservedCall.h"
 #include "../ExecutorBaseTemplated.h"
+#include "../../libraries/Mapper.h"
 
 namespace Executors::Reserved {
 	class ReservedCallExecutor : public ExecutorBaseT<InstructionDefs::Reserved::ReservedCall> {
+	private:
+		std::shared_ptr<Emulation::Libraries::Mapper> _mapper;
 	public:
 		explicit ReservedCallExecutor(const std::shared_ptr<AArch64Cpu> &cpu);
+		ReservedCallExecutor(const std::shared_ptr<AArch64Cpu> &cpu, const std::shared_ptr<Emulation::Libraries::Mapper>& mapper);
 		void execute(const InstructionDefs::Reserved::ReservedCall& instruction) override;
 	};
 }
