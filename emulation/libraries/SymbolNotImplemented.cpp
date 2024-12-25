f096d1d822fc28da40a235ff43f5a431cf988210
Dawid <dawid@dpago.dev>
2024-12-25 19:59:09 +0100

    Make SymbolNotImplemented throw an exception

--- emulation/libraries/SymbolNotImplemented.cpp
+++ emulation/libraries/SymbolNotImplemented.cpp
@@ -1,8 +1,12 @@
 #include "SymbolNotImplemented.h"
+#include <format>
 
 namespace Emulation::Libraries {
 	void SymbolNotImplemented::execute(AArch64Cpu& cpu) {
 		std::cout << "[SymbolNotImplemented] Symbol called: " << this->_symbolName << std::endl;
+		throw std::runtime_error(
+			std::format("The CPU tried to call a function: \"{}\", but there was no implementation provided",
+			this->_symbolName));
 	}
 
 	SymbolNotImplemented::SymbolNotImplemented(const char *symbolName) :
