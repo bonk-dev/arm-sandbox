32ceec64c3f9b458dc3e637d21dc1c0a205d5ea9
bonk-dev <github.p0j6t@passinbox.com>
2024-12-31 14:08:15 +0100

    Implement set flags for ADD/SUB imm.

--- emulation/executors/data_proc_imm/AddSubImmediateExecutor.cpp
+++ emulation/executors/data_proc_imm/AddSubImmediateExecutor.cpp
@@ -1,4 +1,26 @@
 #include "AddSubImmediateExecutor.h"
+#include "../../nzcv.h"
+
+namespace {
+	template<typename IntType>
+	IntType add_with_carry(IntType a, IntType b, bool carry) {
+		uint64_t discarded_nzcv = 0;
+		return add_with_carry(a, b, carry, discarded_nzcv);
+	}
+
+	template<typename UnsignedIntType, typename SignedIntType>
+	UnsignedIntType add_with_carry(UnsignedIntType a, UnsignedIntType b, bool carry, uint64_t& nzcv) {
+		const UnsignedIntType u_sum = a + b + static_cast<unsigned int>(carry);
+		const UnsignedIntType s_sum = static_cast<SignedIntType>(a) + static_cast<SignedIntType>(b) + static_cast<unsigned int>(carry);
+
+		nzcv = NZCV::Negative(nzcv, s_sum < 0);
+		nzcv = NZCV::Zero(nzcv, u_sum == 0);
+		nzcv = NZCV::Carry(nzcv, u_sum < a);
+		nzcv = NZCV::Overflow(nzcv, s_sum < static_cast<SignedIntType>(a));
+
+		return u_sum;
+	}
+}
 
 Executors::DataProcImm::AddSubImmediateExecutor::AddSubImmediateExecutor(const std::shared_ptr<AArch64Cpu> &cpu)
 	: ExecutorBaseT<InstructionDefs::DataProcImm::AddImmediate>(cpu) {}
@@ -15,21 +37,32 @@ void Executors::DataProcImm::AddSubImmediateExecutor::execute(const InstructionD
         imm <<= 12;
     }
 
-    if (instruction.is_subtraction) {
-        val -= imm;
-    }
-    else {
-        val += imm;
-    }
+	uint64_t result;
+	if (instruction.is_subtraction) {
+		// U2
+		imm = ~(imm) + 1;
+	}
+
+	uint64_t nzcv = 0;
+	if (instruction.is_64bit) {
+		result = add_with_carry<uint64_t, int64_t>(val, imm, false, nzcv);
+	}
+	else {
+		result = add_with_carry<uint32_t, int32_t>(val, imm, false, nzcv);
+	}
 
     if (instruction.set_flags) {
-        throw std::runtime_error("Set flags is not implemented");
+		this->get_cpu()->writeNzcvRegsiter(nzcv);
     }
 
-    if (instruction.is_64bit) {
-		cpu->writeGpRegister64(instruction.destination_reg_index, val);
-    }
-    else {
-		cpu->writeGpRegister32(instruction.destination_reg_index, val);
-    }
+	// SP is not writeable using ADD/SUB
+	// it's used as the destination then the result is to be discarded, for example when comparing values to 0 (CMP Xn, #0)
+	if (static_cast<Emulation::Registers>(instruction.destination_reg_index) != Emulation::Registers::Sp) {
+		if (instruction.is_64bit) {
+			cpu->writeGpRegister64(instruction.destination_reg_index, result);
+		}
+		else {
+			cpu->writeGpRegister32(instruction.destination_reg_index, result);
+		}
+	}
 }
