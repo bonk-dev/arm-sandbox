bc45b92b80932b8893d663752007b58a99376775
bonk-dev <github.p0j6t@passinbox.com>
2025-01-05 19:25:42 +0100

    Pass cpu reference to calc_next_address instead of shared_ptr

--- disassembly/instructions/loads_and_stores/indexing_helper.h
+++ disassembly/instructions/loads_and_stores/indexing_helper.h
@@ -8,14 +8,14 @@ namespace InstructionDefs::IndexingHelpers {
 	template <class T>
 	uintptr_t calc_next_address(
 			InstructionDefs::IndexingMode mode,
-			std::shared_ptr<AArch64Cpu>& cpu,
+			AArch64Cpu& cpu,
 			T immediate,
 			regindex_t base_reg,
 			bool is_wide = true
 	) {
 		uintptr_t virt_addr = is_wide
-							  ? cpu->readGpRegister64(base_reg)
-							  : cpu->readGpRegister32(base_reg);
+							  ? cpu.readGpRegister64(base_reg)
+							  : cpu.readGpRegister32(base_reg);
 
 		switch (mode) {
 			case InstructionDefs::IndexingMode::NonTemporalOffset:
@@ -24,11 +24,11 @@ namespace InstructionDefs::IndexingHelpers {
 				virt_addr += immediate;
 				break;
 			case InstructionDefs::IndexingMode::PostIndex:
-				cpu->writeGpRegister64(base_reg, virt_addr + immediate);
+				cpu.writeGpRegister64(base_reg, virt_addr + immediate);
 				break;
 			case InstructionDefs::IndexingMode::PreIndex:
 				virt_addr += immediate;
-				cpu->writeGpRegister64(base_reg, virt_addr);
+				cpu.writeGpRegister64(base_reg, virt_addr);
 				break;
 			default:
 				throw std::runtime_error("Illegal indexing mode");
