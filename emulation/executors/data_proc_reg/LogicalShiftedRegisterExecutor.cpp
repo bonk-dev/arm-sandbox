279f0bbc0d2b3244454098c2a21e36ed5b434ad4
bonk-dev <github.p0j6t@passinbox.com>
2025-01-05 19:25:14 +0100

    Use AArch64Cpu reference in data proc. reg. executors

--- emulation/executors/data_proc_reg/LogicalShiftedRegisterExecutor.cpp
+++ emulation/executors/data_proc_reg/LogicalShiftedRegisterExecutor.cpp
@@ -2,17 +2,14 @@
 
 #include <limits>
 
-Executors::DataProcReg::LogicalShiftedRegisterExecutor::LogicalShiftedRegisterExecutor(
-    const std::shared_ptr<AArch64Cpu> &cpu) : ExecutorBaseT(cpu){}
-
 void Executors::DataProcReg::LogicalShiftedRegisterExecutor::execute(
-    const InstructionDefs::DataProcReg::LogicalShiftedRegister &details) {
+    const InstructionDefs::DataProcReg::LogicalShiftedRegister &details, AArch64Cpu& cpu) {
     if (!details.is64Bit && details.shiftAmount >= 32) {
         throw std::runtime_error("32-bit logical shifted reg. optional shift amount needs to be in the 0-31 range");
     }
 
-    uint64_t first = details.operand1Reg == 31 ? 0 : get_cpu()->readGpRegister64(details.operand1Reg);
-    uint64_t second = details.operand2Reg == 31 ? 0 : get_cpu()->readGpRegister64(details.operand2Reg);
+    uint64_t first = details.operand1Reg == 31 ? 0 : cpu.readGpRegister64(details.operand1Reg);
+    uint64_t second = details.operand2Reg == 31 ? 0 : cpu.readGpRegister64(details.operand2Reg);
 
     if (!details.is64Bit) {
         first &= std::numeric_limits<uint32_t>::max();
@@ -70,9 +67,9 @@ void Executors::DataProcReg::LogicalShiftedRegisterExecutor::execute(
     }
 
     if (details.is64Bit) {
-        get_cpu()->writeGpRegister64(details.destinationReg, result);
+        cpu.writeGpRegister64(details.destinationReg, result);
     }
     else {
-        get_cpu()->writeGpRegister32(details.destinationReg, result);
+        cpu.writeGpRegister32(details.destinationReg, result);
     }
 }
