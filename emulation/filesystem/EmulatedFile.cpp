6185d4c31c5aa9c9472eef0b26f476cb900438f8
bonk-dev <github.p0j6t@passinbox.com>
2025-01-06 13:58:37 +0100

    Make the EmulatedFile ctor manually null-terminate the string

--- emulation/filesystem/EmulatedFile.cpp
+++ emulation/filesystem/EmulatedFile.cpp
@@ -1,8 +1,13 @@
 #include "EmulatedFile.h"
 
 Filesystem::EmulatedFile::EmulatedFile(const std::string& str):
-	Filesystem::File(0), _position(0), _data(str.length()) {
+	Filesystem::File(0),
+	_position(0),
+	_data(str.length() + 1) {
+
 	str.copy(reinterpret_cast<char*>(_data.data()), _data.size());
+	// Ensure the string is null-terminated
+	_data[str.length()] = static_cast<std::byte>('\x00');
 }
 
 std::string Filesystem::EmulatedFile::readString() {
