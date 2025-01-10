4d44d504da29e8429f5fba45864cceab8e23386a
Dawid <dawid@dpago.dev>
2025-01-10 12:21:47 +0100

    Set default log level to verbose for now

--- logging/LoggerFactory.h
+++ logging/LoggerFactory.h
@@ -3,7 +3,7 @@
 #include "StdoutLogger.h"
 
 namespace Logging {
-    inline std::unique_ptr<LoggerBase> createLogger(const char* prefix, LogLevel level = LogLevel::Info) {
+    inline std::unique_ptr<LoggerBase> createLogger(const char* prefix, LogLevel level = LogLevel::Verbose) {
         return std::make_unique<StdoutLogger>(prefix, level);
     }
 }
