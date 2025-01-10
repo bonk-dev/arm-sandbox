07317c56e5d2661a880b6cad91acac5b40815103
Dawid <dawid@dpago.dev>
2025-01-10 13:29:33 +0100

    Remove hardcoded prefix from SymbolNotImplemented

--- emulation/libraries/SymbolNotImplemented.cpp
+++ emulation/libraries/SymbolNotImplemented.cpp
@@ -3,7 +3,7 @@
 
 namespace Emulation::Libraries {
 	void SymbolNotImplemented::execute(AArch64Cpu& cpu) {
-		_logger->error() << "[SymbolNotImplemented] Symbol called: " << this->_symbolName << std::endl;
+		_logger->error() << "Symbol called: " << this->_symbolName << std::endl;
 		throw std::runtime_error(
 			std::format("The CPU tried to call a function: \"{}\", but there was no implementation provided",
 			this->_symbolName));
