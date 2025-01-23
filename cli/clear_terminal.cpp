a9dd9622069c2f5ea09df9a20fa421db55908c6d
bonk-dev <github.p0j6t@passinbox.com>
2025-01-23 14:16:07 +0100

    Implement clear_terminal for Windows

--- cli/clear_terminal.cpp
+++ cli/clear_terminal.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
 #include "clear_terminal.h"
+#if defined(_WIN32) or defined(WIN32)
+#include <windows.h>
+#endif
 
 void clear_terminal() {
 #if defined(_WIN32) or defined(WIN32)
-	static_assert(false, "clear_console not implemented");
+	//https://gist.github.com/karolisjan/6e4d33211847b0eb77317ab186b3840b
+
+	// Get the Win32 handle representing standard output.
+	// This generally only has to be done once, so we make it static.
+	static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	COORD topLeft = { 0, 0 };
+
+	// std::cout uses a buffer to batch writes to the underlying console.
+	// We need to flush that to the console because we're circumventing
+	// std::cout entirely; after we clear the console, we don't want
+	// stale buffered text to randomly be written out.
+	std::cout.flush();
+
+	// Figure out the current width and height of the console window
+	if (!GetConsoleScreenBufferInfo(hOut, &csbi)) {
+		// TODO: Handle failure!
+		abort();
+	}
+	DWORD length = csbi.dwSize.X * csbi.dwSize.Y;
+
+	DWORD written;
+
+	// Flood-fill the console with spaces to clear it
+	FillConsoleOutputCharacter(hOut, TEXT(' '), length, topLeft, &written);
+
+	// Reset the attributes of every character to the default.
+	// This clears all background colour formatting, if any.
+	FillConsoleOutputAttribute(hOut, csbi.wAttributes, length, topLeft, &written);
+
+	// Move the cursor back to the top left for the next sequence of writes
+	SetConsoleCursorPosition(hOut, topLeft);
 #elif defined(__unix) or defined(__unix__)
 	// CSI[2J clears
 	// CSI[H moves cursor to top left
