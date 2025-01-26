18bd57ffd6c5debc524a2660f689a60bd8addf14
bonk-dev <github.p0j6t@passinbox.com>
2025-01-26 11:59:42 +0100

    Add comments for logging utils

--- logging/utils.h
+++ logging/utils.h
@@ -1,6 +1,17 @@
 #pragma once
 
 namespace Logging {
+	/**
+	 * @brief Parses an std::string into a Logging::LogLevel
+	 * @param s The string to be parsed
+	 * @return Parsed Logging::LogLevel
+	 */
 	Logging::LogLevel str_to_log_level(const std::string &s);
+
+	/**
+	 * @brief Converts a Logging::LogLevel to a c-string
+	 * @param level The log level to be stringified
+	 * @return The string representation of the log level
+	 */
 	const char* log_level_to_str(Logging::LogLevel level);
 }
