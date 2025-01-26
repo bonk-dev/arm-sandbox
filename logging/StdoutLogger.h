014dc175cc1518add06aa25b631c92ccd59c9cba
bonk-dev <github.p0j6t@passinbox.com>
2025-01-26 12:05:34 +0100

    Add a comment for the StdoutLogger class

--- logging/StdoutLogger.h
+++ logging/StdoutLogger.h
@@ -2,6 +2,9 @@
 #include "LoggerBase.h"
 
 namespace Logging {
+	/**
+	 * @brief A logger implementation which outputs messages to stdout (and stderr)
+	 */
     class StdoutLogger : public LoggerBase {
     protected:
         std::ostream& getStream() override;
