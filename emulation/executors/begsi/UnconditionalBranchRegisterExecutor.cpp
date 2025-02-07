2d2857d811e83d1d4064d9430d94ebbea050de3c
bonk-dev <github.p0j6t@passinbox.com>
2025-02-07 23:36:14 +0100

    Use the new writeRegister functions in begsi

--- emulation/executors/begsi/UnconditionalBranchRegisterExecutor.cpp
+++ emulation/executors/begsi/UnconditionalBranchRegisterExecutor.cpp
@@ -3,7 +3,7 @@
 void
 Executors::Begsi::UnconditionalBranchRegisterExecutor::execute(
 		const InstructionDefs::Begsi::UnconditionalBranchRegister &instruction, AArch64Cpu& cpu) {
-	const virtual_address_t target = cpu.readRegister64(instruction.destination_reg);
+	const virtual_address_t target = cpu.readRegister(instruction.destination_reg, 64);
 	this->branchTo(
 			target,
 			Emulation::BranchType::IndirectCall,
